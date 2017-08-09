33267c73ffd3075dd836d363d6d853a8f71e5c77
Daniel S Choi <daniel.won.choi@gmail.com>
2017-08-08 23:43:10 -0700

    score and trigger done

---
+++ Assets/Daniel-Code/GoalController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalController : MonoBehaviour {
+
+    private BoxCollider2D goal;
+    public Text scoreText;
+    private int scoreCount;
+
+	// Use this for initialization
+	void Start () {
+        goal = GameObject.FindGameObjectWithTag("goal").GetComponent<BoxCollider2D>();
+        scoreCount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        scoreCount++;
+        updateScore();
+    }
+
+    void updateScore()
+    {
+        scoreText.text = "" + scoreCount;
+    }
+}
