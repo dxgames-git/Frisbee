0261f11f82b226094f8d4df31c427d109f35a774
Daniel S Choi <daniel.won.choi@gmail.com>
2017-08-09 22:11:33 -0700

    width of the goal changes
    -time based frisbee game
    - as of now, the width changes whenever the upArrow key is pressed

--- Assets/Daniel-Code/GoalController.cs
+++ Assets/Daniel-Code/GoalController.cs
@@ -8,16 +8,22 @@ public class GoalController : MonoBehaviour {
     private BoxCollider2D goal;
     public Text scoreText;
     private int scoreCount;
+    private float count;
 
 	// Use this for initialization
 	void Start () {
         goal = GameObject.FindGameObjectWithTag("goal").GetComponent<BoxCollider2D>();
         scoreCount = 0;
+        count = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            count -= 0.1f;
+            changeWidth(count);
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -30,5 +36,13 @@ public class GoalController : MonoBehaviour {
     void updateScore()
     {
         scoreText.text = "" + scoreCount;
+    }
+
+    void changeWidth(float count)
+    {
+        Vector3 changeSize = new Vector3(transform.localScale.x + count, transform.localScale.y); //stop decreasing the size of the width when it hits a certain extent
+        transform.localScale = changeSize;
+        Vector2 s = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        gameObject.GetComponent<BoxCollider2D>().size = s;
     }
 }
