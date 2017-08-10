41e0ecb2d7968c27ffa91aa59fb934699d3d31b7
Heesoo Home <heesoo4199@gmail.com>
2017-08-09 23:13:39 -0700

    Frisbee Spawning

--- Assets/Scripts/Frisbee.cs
+++ Assets/Scripts/Frisbee.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Frisbee : MonoBehaviour {
 
+    public bool shot;
+
     public float speed;
 
     // Use this for initialization
     void Start () {
         //GetComponent<Renderer>().material.color = new Color(0, 255, 0); //C sharp
+        shot = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            shot = true;
             float z = Mathf.Cos(Mathf.PI / 6f) * speed;
             float y = Mathf.Sin(Mathf.PI / 6f) * speed;
             print(z);
             print(y);
             gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, y, z);
+            GameObject.FindGameObjectWithTag("FrisbeeController").GetComponent<FrisbeeController>().spawnDisc(((int) (Random.value * 3f)) - 1);
             Destroy(this);
         }
     }
---
+++ Assets/Scripts/FrisbeeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrisbeeController : MonoBehaviour {
+
+    /// <summary>
+    /// This class handles Frisbee spawning and sideways oscillation.
+    /// </summary>
+
+    // The speed at which the disc oscillates left and right
+    public float sidewaySpeed;
+
+    // The absolute value of the amplitude of horizontal oscillation 
+    public float sidewayRange;
+
+    // The prefab of the disc being spawned
+    public GameObject discPrefab;
+
+    public GameObject currentDisc;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // If the frisbee hasn't been shot yet, then its Frisbee.cs script won't be null
+        if (currentDisc.GetComponent<Frisbee>().shot == false)
+        {
+            if (currentDisc.transform.position.x > sidewayRange)
+            {
+                currentDisc.GetComponent<Rigidbody>().velocity = new Vector2(-sidewaySpeed, 0f);
+            }
+            else if (currentDisc.transform.position.x < -sidewayRange)
+            {
+                currentDisc.GetComponent<Rigidbody>().velocity = new Vector2(sidewaySpeed, 0f);
+            }
+        }		
+	}
+
+    // Spawns a disc: -1 for left, 1 for right
+    public void spawnDisc(int side)
+    {
+        GameObject newDisc = Instantiate(discPrefab);
+        newDisc.transform.position = new Vector3(side * 2f, 0f, -7f);
+        currentDisc = newDisc;
+    }
+
+}
